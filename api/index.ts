7d26d0444cdb92560047d0d1cdc232e1c641dd90
Zhuravlev Petr <zeterfrost@gmail.com>
2024-01-16 15:31:18 +0600

    add api

---
+++ api/index.ts
@@ -0,0 +1,29 @@
+import express, { json } from 'express';
+
+const app = express();
+const port = 8000;
+const Vigenere = require('caesar-salad').Vigenere;
+
+app.use(json());
+
+app.post('/encode', (req, res) => {
+	const password = req.body.password;
+	const message = req.body.message;
+	const answer = {
+		encoded: Vigenere.Cipher(password).crypt(message),
+	};
+	res.send(answer);
+});
+
+app.post('/decode', (req, res) => {
+	const password = req.body.password;
+	const message = req.body.message;
+	const answer = {
+		decoded: Vigenere.Decipher(password).crypt(message),
+	};
+	res.send(answer);
+});
+
+app.listen(port, () => {
+	console.log(`Server started on ${port} port!`);
+});
